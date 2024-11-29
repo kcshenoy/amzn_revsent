453f129bf1f5820dbababb26ae522fedbee99ab4
kcshenoy <krishna.cshenoy@gmail.com>
2024-11-29 12:06:29 -0500

    remove unfinished auth functionality

--- backend/auth/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
-
-class SignupForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
